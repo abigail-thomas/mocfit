2f2ec11084e6207ed584fdce09a50e808bce8449
CaptainBacon22 <aheger@mocs.flsouthern.edu>
2025-09-14 12:45:56 -0400

    changed html names

--- workouts/urls.py
+++ workouts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name = "home"),
-    path("<str:workout_type>/", views.generate, name = "generate"),
+    path("", views.workout_generator_page, name = "workout_generator_page"),
+    path("<str:workout_type>/", views.workout_generator, name = "workout_generator"),
 ]
--- workouts/views.py
+++ workouts/views.py
@@ -5,16 +5,16 @@ from .data import workout_list
 # Create your views here.
 
 
-def home(request):
-    return render(request, "workouts/home.html")
+def workout_generator_page(request):
+    return render(request, "workouts/workout_generator_page.html")
 
-def generate(request, workout_type):
+def workout_generator(request, workout_type):
     exercises = workout_list.get(workout_type, [])
 
     num_to_pick = random.choice([3, 4])
     chosen = random.sample(exercises, k = num_to_pick) if exercises else []
 
-    return render(request, "workouts/result.html", {
+    return render(request, "workouts/workout_results.html", {
         "type": workout_type.capitalize(),
         "exercises": chosen
     })
