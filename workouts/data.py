1f1806ccd7572395ae52423eef6ae363c180e2ed
CaptainBacon22 <aheger@mocs.flsouthern.edu>
2025-09-14 12:13:18 -0400

    cleaned up master

--- workouts/data.py
+++
@@ -1,8 +0,0 @@
-# workout data
-
-workout_list = {
-    "push": ["Bench Press", "Overhead Press", "Push-ups", "Dips", "Incline Dumbbell Press", "Chest Fly"],
-    "pull": ["Pull-ups", "Barbell Row", "Bicep Curls", "Face Pulls", "Lat Pulldown", "Hammer Curl"],
-    "legs": ["Squats", "Lunges", "Deadlifts", "Leg Press", "Leg Extensions", "Calf Raises"],
-    "arms": [],
-}
