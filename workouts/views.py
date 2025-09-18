08f801f47c333c4547396a801d149970a80c4edf
CaptainBacon22 <aheger@mocs.flsouthern.edu>
2025-09-18 17:11:57 -0400

    workout generator now pulls from models database

--- workouts/views.py
+++ workouts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import random
 from .data import workout_list, advanced_workout_list
+from .models import Exercise, MuscleGroup
 
 # Create your views here.
 
@@ -23,6 +24,22 @@ def advanced_workout_generator(request):
     if request.method == "POST":
         selected_muscles = request.POST.getlist("muscles")
         generated = {}
-        for muscle in selected_muscles:
-            generated[muscle] = advanced_workout_list.get(muscle, [])
+
+        for muscle_name in selected_muscles:
+            try:
+                muscle = MuscleGroup.objects.get(name = muscle_name)
+                exercises = Exercise.objects.filter(muscles = muscle)
+                generated[muscle.name] = exercises
+            except MuscleGroup.DoesNotExist:
+                generated[muscle_name] = []
+        
         return render(request, "workouts/advanced_workout_results.html", {"workout": generated})
+
+# this function pulled from the workout list in data.py instead of the database in models.py
+# def advanced_workout_generator(request):
+#     if request.method == "POST":
+#         selected_muscles = request.POST.getlist("muscles")
+#         generated = {}
+#         for muscle in selected_muscles:
+#             generated[muscle] = advanced_workout_list.get(muscle, [])
+#         return render(request, "workouts/advanced_workout_results.html", {"workout": generated})
