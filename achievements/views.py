c4b29a1efc11825d390927cc46ec0ed3dbfdd89e
Joel Peters <joelpeters2017@gmail.com>
2025-10-07 17:25:50 -0400

    updated everything in achievements

--- achievements/views.py
+++ achievements/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import UserAchievement
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/accounts/')
 def my_achievements(request):
     achievements = UserAchievement.objects.filter(user=request.user)
     return render(request, "achievements/my_achievements.html", {"achievements": achievements})
