48de59de8cbd8ddea0f62290c6734c62b8450f23
MaxwellOlson-Burkman <maxols23@icloud.com>
2025-10-07 11:29:44 -0400

    Added: incroperated the style of the home page into the community section

--- community_page/views.py
+++ community_page/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count
 from .models import Post
 from .forms import PostForm, CommentForm
 
 def index(request):
-    posts = Post.objects.all().order_by('-created_at')
+    # Order posts by number of likes (descending), then by creation date
+    posts = Post.objects.annotate(
+        like_count=Count('likes')
+    ).order_by('-like_count', '-created_at')
+    
     comment_form = CommentForm()
     return render(request, 'community_page/index.html', {
         'posts': posts,
