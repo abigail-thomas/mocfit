213c385579c83ff6ee39aba5bff13f4dc66cfcf8
CaptainBacon22 <aheger@mocs.flsouthern.edu>
2025-11-13 16:19:23 -0500

    fix workout view issue

--- workouts/views.py
+++ workouts/views.py
@@ -12,6 +12,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
+from django.shortcuts import render
+
 # Create your views here.
 
 
